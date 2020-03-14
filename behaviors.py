bcbd366b957aeca0b8d77a56517d5dfb4bfd708b
Simon Katzer <skatzer@ucsc.edu>
2020-03-13 19:34:38 -0700

    Added some more checks/behaviors

--- behaviors.py
+++ behaviors.py
@@ -1,3 +1,29 @@
 
 def trivial_behavior(state):
     return True
+
+################### CAT STUFF ###################
+
+# Maybe we make these behaviors update other factors (eg. play increases tiredness)?
+
+def cat_meow(state):
+    print("Your cat meows!")
+    return True
+
+def cat_play_alone(state):
+    print("Your cat tries to catch an imaginary bug")
+    return True
+
+def cat_groom_self(state):
+    print("Your cat is grooming itself")
+    return True
+
+def cat_sleep(state):
+    print("Your cat is sleeping")
+    return True
+
+################### FISH STUFF ###################
+
+def fish_idle(state):
+    print("Your fish doesn't appear to be doing anything out of the ordinary")
+    return True
--- checks.py
+++ checks.py
@@ -2,16 +2,20 @@
 def trivial_check(state):
     return True
 
-def can_eat_from_bowl(state): # Used by cat & dog (since they share a food bowl)
-    for bowl in state.food_bowls:
-        if bowl > 10:
-            return true
-    return false
+################### CAT & DOG SHARED STUFF #####################
 
-def is_cat_sociable(state): # Assumes state.cat is the only cat we care about
+def can_eat_from_bowl(state): # Used by cat & dog (since they share a food bowl)
+    return state.food_bowl > 10
+
+################### CAT STUFF ###################
+
+def cat_is_sociable(state): # Assumes state.cat is the only cat we care about
     return state.priority(state.cat.meter["social"]) > 60
 
-def is_dog_sociable(state): # Assumes state.cat is the only cat we care about
-    return state.priority(state.dog.meter["social"]) > 30
+def cat_is_litter_box_full(state):
+    return state.litter_box > 10
 
-print(moo)
+################### DOG STUFF ###################
+
+def dog_is_sociable(state): # Assumes state.cat is the only cat we care about
+    return state.priority(state.dog.meter["social"]) > 30
